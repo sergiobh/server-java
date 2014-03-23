87f517f53d357bb91262f46c0d833fc215fc9c34
Sérgio <sergiobh@gmail.com>
2014-03-23 19:03:30 -0300

    auditoria estudos para prova

---
+++ AuditoriaQualidade/Prova01/src/excecao/EmprestimoException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excecao;
+
+/**
+ *
+ * @author edgardcardoso
+ */
+public class EmprestimoException extends Exception{
+    public EmprestimoException(String msg){
+        super(msg);
+    }
+    
+}
